be95f54805e87ec3244aab6a528abbe510e0783b
sethfisher <sethfisher@gmail.com>
2017-12-12 18:52:27 -0500

    Initial commit

---
+++ web/sites/default/settings.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Salt for one-time login links, cancel links, form tokens, etc.
+ */
+$settings['hash_salt'] = 'WH76ArLQ7i8cSbCoEaZnA4jwz0ImsnlDG4Fbg9t6BV409Ehjas0cOvvVb92Ok7Cor0isF8PQzw';
+
+/**
+ * Access control for update.php script.
+ */
+$settings['update_free_access'] = FALSE;
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+
+/**
+ * The default list of directories that will be ignored by Drupal's file API.
+ *
+ * @see file_scan_directory()
+ * @see \Drupal\Core\Extension\ExtensionDiscovery::scanDirectory()
+ */
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+
+/**
+ * The default number of entities to update in a batch process.
+ */
+$settings['entity_update_batch_size'] = 50;
+
+$config_directories['sync'] = '../config/sync';
+
+$settings['install_profile'] = 'standard';
+
+/**
+ * Load local development override configuration, if available.
+ * Keep this code block at the end of this file to take full effect.
+ */
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
